3aa266e496d41a6c0ac9ceee8105d72457d400db
Luuk de Boer <129878@hpleerling.nl>
2016-12-20 08:31:34 +0100

    code komt overeen met naam

--- read.php
+++ read.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select file to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload file" name="submit">
-</form>
-
-</body>
-</html>
